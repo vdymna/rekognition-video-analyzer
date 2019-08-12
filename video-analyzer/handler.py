77fc87a0ca1c8cfd08ccb7b3fa4ff6f7d12bd641
Vitaliy Dymna <vitaliy.dymna@xcentium.com>
2019-08-12 18:08:06 -0500

    Added initial handler function code .

--- video-analyzer/handler.py
+++ video-analyzer/handler.py
@@ -1,4 +1,24 @@
-def start_processing_video(event, context):
-    print(event)
+import urllib
+import boto3
 
+
+def start_processing_video(event, context):
+    for record in event['Records']:
+        start_label_detection(
+            record['s3']['bucket']['name'], 
+            urllib.parse.unquote_plus(record['s3']['object']['key'])
+        )
+
+    return
+
+def start_label_detection(bucket_name, key):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.start_label_detection(
+        Video={
+            'S3Object': { 
+                'Bucket': bucket_name, 
+                'Name': key 
+            }
+        })
+    
     return
